f4590db7ebf93931333f84ce6db624fa112556c1
RitwishaInCode <149022876+RitwishaInCode@users.noreply.github.com>
2023-12-30 01:34:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Todo from "./components/todo"
+
+//rafc - shortcut to bring the boiler code
+const App = () => {
+
+ 
+
+  return (
+    <div>
+    <Todo/>
+
+    </div>
+  )
+}
+
+export default App
